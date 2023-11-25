c7efd67763f9a0c4f852f69af109f38adf9ae4cb
CamFred <149436006+CamFred@users.noreply.github.com>
2023-11-25 20:02:50 +0000

    added tiers.

--- script.js
+++ script.js
@@ -27,28 +27,41 @@ document.addEventListener('DOMContentLoaded', function() {
         // Create and display inventory items
         inventoryModules.forEach(moduleObj => {
             if (!equippedModules.has(moduleObj.name)) {
-                let div = createModuleDiv(moduleObj.name, moduleObj.stats);
+                // Determine the tier of the module (you'll need a way to do this)
+                // For example, assuming you have a 'tier' property in each module object
+                const tier = moduleObj.tier;
+        
+                // Call createModuleDiv with the tier parameter
+                let div = createModuleDiv(moduleObj.name, moduleObj.stats, tier);
                 div.addEventListener('dragstart', handleDragStart);
                 inventory.appendChild(div);
             }
         });
     }
 
-    function createModuleDiv(moduleName, moduleStats) {
+    function createModuleDiv(moduleName, moduleStats, tier) {
         let div = document.createElement('div');
         div.className = 'module';
         div.textContent = moduleName;
         div.draggable = true;
     
+        // Replace spaces with hyphens in the tier to create a valid class name
+        const tierClass = tier.replace(/\s+/g, '-').toLowerCase();
+        
+        // Add a class based on the modified tier
+        //console.log("I am adding module-tier-" + tierClass);
+        div.classList.add(`module-${tierClass}`);
+    
         // Add click event listener to display module stats
         div.addEventListener('click', () => {
-            displayModuleStats(moduleName, moduleStats);
+            displayModuleStats(moduleName, moduleStats, tier);
         });
     
         return div;
     }
+    
 
-    function displayModuleStats(moduleName, stats) {
+    function displayModuleStats(moduleName, stats, tier) {
         // Clear previous module stats
         moduleStats.innerHTML = '';
     
@@ -59,10 +72,65 @@ document.addEventListener('DOMContentLoaded', function() {
     
         // Create a div for displaying the stats
         const statsDiv = document.createElement('div');
-        statsDiv.textContent = JSON.stringify(stats, null, 2);
+    
+        // Display the tier information
+        const tierParagraph = document.createElement('p');
+        tierParagraph.textContent = `Tier: ${tier}`;
+        statsDiv.appendChild(tierParagraph);
+    
+        // Loop through the stats object and display each stat
+        for (const [statName, statValue] of Object.entries(stats)) {
+            const statLine = document.createElement('p');
+            const formattedStatName = prettyText(statName);
+            statLine.textContent = `${formattedStatName}: ${statValue}`;
+            statsDiv.appendChild(statLine);
+        }
+    
         moduleStats.appendChild(statsDiv);
     }
     
+    // Function to update the ship's stats based on equipped modules
+function updateShipStats() {
+    const shipStatsDiv = document.getElementById('ship-stats');
+    shipStatsDiv.innerHTML = ''; // Clear previous ship stats
+
+    // Initialize an object to store ship stats
+    const shipStats = {};
+
+    // Iterate through equipped modules
+    equippedModules.forEach(moduleName => {
+        const moduleObj = inventoryModules.find(obj => obj.name === moduleName);
+        if (moduleObj && moduleObj.stats) {
+            // Iterate through the stats of the module and add them to shipStats
+            for (const [statName, statValue] of Object.entries(moduleObj.stats)) {
+                // Use the statName as the key and add the statValue to shipStats
+                if (shipStats.hasOwnProperty(statName)) {
+                    // If the stat already exists, add the new value
+                    shipStats[statName] += statValue;
+                } else {
+                    // If the stat doesn't exist, create it with the new value
+                    shipStats[statName] = statValue;
+                }
+            }
+        }
+    });
+
+    // Create a list of ship stats and their values
+    const shipStatsList = document.createElement('ul');
+    for (const [statName, statValue] of Object.entries(shipStats)) {
+        const listItem = document.createElement('li');
+        const formattedStatName = prettyText(statName);
+        listItem.textContent = `${formattedStatName}: ${statValue}`;
+        shipStatsList.appendChild(listItem);
+    }
+
+    shipStatsDiv.appendChild(shipStatsList);
+}
+
+// Call the updateShipStats function initially to display ship stats
+updateShipStats();
+
+
 
     function handleDragStart(event) {
         const moduleName = event.target.textContent;
@@ -110,9 +178,13 @@ document.addEventListener('DOMContentLoaded', function() {
             moduleDiv.textContent = moduleName;
             moduleDiv.draggable = true;
     
+            // Add the module tier class to the bay module
+            const tierClass = moduleObj.tier.replace(/\s+/g, '-').toLowerCase();
+            moduleDiv.classList.add(`module-${tierClass}`);
+    
             // Add click event listener to display module stats
             moduleDiv.addEventListener('click', () => {
-                displayModuleStats(moduleName, moduleObj.stats);
+                displayModuleStats(moduleName, moduleObj.stats, moduleObj.tier);
             });
     
             // Append the new module element to the bay
@@ -123,8 +195,12 @@ document.addEventListener('DOMContentLoaded', function() {
     
             // Re-fetch modules and update the inventory display
             fetchModulesAndUpdateDisplay();
+    
+            // Update the ship's stats
+            updateShipStats();
         }
     }
+    
     
     
     function handleInventoryDrop(event) {
@@ -141,7 +217,16 @@ document.addEventListener('DOMContentLoaded', function() {
 
             // Re-fetch modules and update the inventory display
             fetchModulesAndUpdateDisplay();
+
+           // Update the ship's stats
+            updateShipStats();
         }
+    }
+
+    function prettyText(text) {
+        // Use regular expression to match lowercase letters followed by uppercase letters
+        // and insert a space before the uppercase letter, then capitalize the first letter
+        return text.replace(/([a-z])([A-Z])/g, '$1 $2').replace(/^./, str => str.toUpperCase());
     }
 
     // Function to set up the event listeners for the module bay elements
